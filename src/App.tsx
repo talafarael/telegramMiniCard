dc17d967c108d1e5ba0381e7c2f2cb50f1a30513
artem <127351345+talafarael@users.noreply.github.com>
2024-11-10 22:53:43 +0200

    tg get data

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useLaunchParams } from "@telegram-apps/sdk-react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const lp = useLaunchParams();
+  console.log(lp);
+  return <div className="App">fff</div>;
 }
 
 export default App;
