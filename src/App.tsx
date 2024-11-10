906704becbf971ee56fa0bced2bb0295eaf81d57
artem <127351345+talafarael@users.noreply.github.com>
2024-11-10 23:06:09 +0200

    d

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import { useLaunchParams } from "@telegram-apps/sdk-react";
 import "./App.css";
+const userData={
 
+}
 function App() {
   const lp = useLaunchParams();
-  console.log(lp);
+  console.log(JSON.stringify(lp));
+
   return <div className="App">fff</div>;
 }
 
