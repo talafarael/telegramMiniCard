ac03bb4f8be8b6574413ba36aaabc4bee7c009d8
artem <127351345+talafarael@users.noreply.github.com>
2024-11-18 00:33:01 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -8,9 +8,14 @@ export const userParam =
 function App() {
   // const navigate = useNavigate();
 
+  const [user, setUser] = useState<string>("");
   // searchParams.set(newParamKey, newParamValue);
   const [session, useSession] = useState<string>();
+  const handlerSelectRolle = (data: string) => {
+    setUser(data);
+  };
   useEffect(() => {
+    if (user == "") return;
     const port = "ws://localhost:8080/";
     const ws = new WebSocket(port);
     // const lp = useLaunchParams();
@@ -21,10 +26,9 @@ function App() {
       const message = {
         action: "join",
         roomId: tokenRoom ? tokenRoom : undefined,
-        userData: userParam,
+        userData: user,
       };
       ws.send(JSON.stringify(message));
-
     };
     ws.onmessage = async (event) => {
       const res = JSON.parse(event.data);
@@ -34,7 +38,7 @@ function App() {
           const url = new URL(window.location.href);
           url.searchParams.set("token", res.roomId);
           window.history.replaceState({}, "", url.toString());
-         
+
           break;
         }
       }
@@ -45,12 +49,34 @@ function App() {
     return () => {
       ws.close();
     };
-  }, []);
+  }, [user]);
 
   // const lp = useLaunchParams();
   // console.log(JSON.stringify(lp));
 
-  return <div className="App">fafa</div>;
+  return (
+    <div className="App">
+      <button
+        onClick={() => {
+          handlerSelectRolle(
+            "AAHdF6IQAAAAAN0XohDhrOrc&user=%7B%22id%22%3A1%2C%22first_name%22%3A%22Test%22%2C%22last_name%22%3A%22Testenko%22%2C%22username%22%3A%22tst%22%2C%22language_code%22%3A%22ru%22%2C%22is_premium%22%3Atrue%7D&auth_date=1662771648&hash=c501b71e775f74ce10e377dea85a7ea24ecd640b223ea86dfe453e0eaed2e2b2"
+          );
+        }}
+      >
+        user1
+      </button>
+
+      <button
+        onClick={() => {
+          handlerSelectRolle(
+            "user=%7B%22id%22%3A1056119921%2C%22first_name%22%3A%22farael%22%2C%22last_name%22%3A%22%22%2C%22username%22%3A%22shinerfa%22%2C%22language_code%22%3A%22uk%22%2C%22allows_write_to_pm%22%3Atrue%7D&chat_instance=948078213344090422&chat_type=sender&auth_date=1731334219&hash=d75e77e0a3702152c2845498d621771eedd66724db2a23efeb4d99f0012f3e85"
+          );
+        }}
+      >
+        user2
+      </button>
+    </div>
+  );
 }
 
 export default App;
