4b3f85437f9c882c90cedb317461f20456c1d76a
artem <127351345+talafarael@users.noreply.github.com>
2024-11-25 22:52:02 +0200

    room join

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
 import React, { useEffect, useState } from "react";
 // import { useLaunchParams } from "@telegram-apps/sdk-react";
 import "./App.css";
-
+export interface IUser {
+  session: string;
+  hash: string;
+  allowsWriteToPm: boolean | null | undefined;
+  firstName: string | null;
+  id: number;
+  username: string | null | undefined;
+}
 export const userParam =
   "user=%7B%22id%22%3A1056119921%2C%22first_name%22%3A%22farael%22%2C%22last_name%22%3A%22%22%2C%22username%22%3A%22shinerfa%22%2C%22language_code%22%3A%22uk%22%2C%22allows_write_to_pm%22%3Atrue%7D&chat_instance=948078213344090422&chat_type=sender&auth_date=1731334219&hash=d75e77e0a3702152c2845498d621771eedd66724db2a23efeb4d99f0012f3e85";
 // "query_id=AAHdF6IQAAAAAN0XohDhrOrc&user=%7B%22id%22%3A1%2C%22first_name%22%3A%22Test%22%2C%22last_name%22%3A%22Testenko%22%2C%22username%22%3A%22tst%22%2C%22language_code%22%3A%22ru%22%2C%22is_premium%22%3Atrue%7D&auth_date=1662771648&hash=c501b71e775f74ce10e377dea85a7ea24ecd640b223ea86dfe453e0eaed2e2b2";
 function App() {
-
   const [user, setUser] = useState<string>("");
-  const [session, useSession] = useState<string>();
+  const [palyer, setPlayer] = useState<IUser[] | []>([]);
   const handlerSelectRolle = (data: string) => {
     setUser(data);
   };
@@ -35,7 +41,7 @@ function App() {
           const url = new URL(window.location.href);
           url.searchParams.set("token", res.roomId);
           window.history.replaceState({}, "", url.toString());
-
+          console.log(res);
           break;
         }
       }
@@ -53,7 +59,6 @@ function App() {
 
   return (
     <div className="App">
-
       <button
         onClick={() => {
           handlerSelectRolle(
@@ -75,7 +80,6 @@ function App() {
       </button>
 
       <button>start</button>
-
     </div>
   );
 }
