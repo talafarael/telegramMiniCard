b4235e5cbb3d00db4314160aad2dbb4443505a3b
artem <artemk2504@gmail.com>
2025-02-17 22:12:00 +0200

    Update index.tsx

--- src/components/AuthWeb/index.tsx
+++ src/components/AuthWeb/index.tsx
@@ -3,7 +3,7 @@ import { LoginButton } from '@telegram-auth/react';
 export const AuthWeb = () => {
   return <div className="App">
     <LoginButton
-      botUsername="@talafarelBot"
+      botUsername="talafarelBot"
       onAuthCallback={(data) => {
         console.log(data);
 
